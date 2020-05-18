498c0f331f86f24c69755724b564f3195e80ca88
Jorge Soriano <proyuo1000@gmail.com>
2020-05-18 03:06:06 -0500

    se borra una carpeta para sustituir

--- workspace/TT2/server/build/controllers/usersCrontrollers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class UsersCrontroller {
-    index(req, res) {
-        res.send('Usersssss');
-    }
-}
-const usersCrontroller = new UsersCrontroller();
-exports.default = usersCrontroller;
